ea5a5a5e972ae3762f9c745929a2ec7630659198
Your Name <almannofrancesco@gmail.com>
2023-01-12 16:51:01 +0100

    exercise completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        <img src="../assets/vue.svg" alt="">
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "AppTitle",
+    data() {
+        return {
+            text: 'La mia prima app con Vite'
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        {{ text }}
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
